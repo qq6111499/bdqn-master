7265b2507a2dd3428faea151a34d242295e39075
qq6111499 <6111499@qq.com>
2018-07-04 10:12:09 +0800

    dsa111

---
+++ src/main/java/com/bdqn/rlzyglxt/RlzyglxtApplication.java
@@ -0,0 +1,12 @@
+package com.bdqn.rlzyglxt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RlzyglxtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RlzyglxtApplication.class, args);
+	}
+}
---
+++ src/test/java/com/bdqn/rlzyglxt/RlzyglxtApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bdqn.rlzyglxt;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RlzyglxtApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
